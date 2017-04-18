c4a2a05992cb2b1845d40a63f8f404736d34d66a
Ivan Lisitskiy <lisitckii@embria.com>
2017-04-18 17:51:02 +0300

    try to output data

--- app/DataFrameCsv.php
+++ app/DataFrameCsv.php
@@ -3,18 +3,31 @@
 namespace app;
 
 use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class DataFrameCsv extends \Phpml\Dataset\CsvDataset
 {
+    /** @var OutputInterface */
+    protected $output;
+
+    public function setOutput(OutputInterface $output)
+    {
+        $this->output = $output;
+    }
+
     public function shape()
     {
-        echo new DataFrameShape(count($this->samples), count($this->columnNames));
+        $this->output->writeln([count($this->samples), count($this->columnNames)]);
     }
 
     public function head(int $count = 5)
     {
-        $this->renderTable(array_slice($this->samples, 0, $count));
+        $samples = array_slice($this->samples, 0, $count);
+        $targets = array_slice($this->targets, 0, $count);
+        foreach ($targets as $key => $val) {
+            array_push($samples[$key], $val);
+        }
+        $this->renderTable($samples);
     }
 
     public function tail(int $count = 5)
@@ -22,10 +35,19 @@ class DataFrameCsv extends \Phpml\Dataset\CsvDataset
         $this->renderTable(array_slice($this->samples, -$count));
     }
 
+    public function setColumnNames(array $columnNames)
+    {
+        $this->columnNames = $columnNames;
+    }
+
+    public function setColumnName(int $index, string $name)
+    {
+        $this->columnNames[$index] = $name;
+    }
+
     protected function renderTable($data)
     {
-        $output = new ConsoleOutput();
-        $table = new Table($output);
+        $table = new Table($this->output);
         $table->setHeaders($this->getColumnNames());
         $table->addRows($data);
         $table->render();
--- prepend.php
+++ prepend.php
@@ -6,3 +6,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 $data = new \app\DataFrameCsv(__DIR__ . '/data/crx.data', 15, false);
 
+$output = new ConsoleOutput();
+$data->setOutput($output);
+
